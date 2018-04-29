065e2a3c4165001b018ae90426297f6c9e1e01d4
Gabriel Kanev <contact@mrgkanev.eu>
2018-04-29 19:59:47 +0300

    Tweaking
    Yes a lot of work is going to be done here don't worry.

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -15,7 +15,7 @@
 ###################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-# 						IT CAN BRAKE THINGS
+#					IT CAN BRAKE THINGS
 #
 ###################################################################
 
@@ -25,7 +25,7 @@ sudo apt-get dist-upgrade -y
 sudo apt-get upgrade -y
 
 #install required software
-sudo apt-get install curl git-core build-essential autoconf postgresql mutt parcellite alarm-clock gtimelog irssi shutter vlc htop git-gui vifm libgmp3c2 libgmpxx4ldbl libgmp3-dev imagemagick libmagickcore-dev libmagickcore4 libmagickcore4-extra libmagickwand-dev libmagickwand4 -y
+sudo apt-get install curl git-core build-essential autoconf postgresql mutt parcellite alarm-clock gtimelog irssi shutter htop git-gui vifm libgmp3c2 libgmpxx4ldbl libgmp3-dev imagemagick libmagickcore-dev libmagickcore4 libmagickcore4-extra libmagickwand-dev libmagickwand4 -y
 sudo apt-get install meld autojump trayer xloadimage banshee gnome-do beanstalkd redis-server -y
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
@@ -36,5 +36,11 @@ git config --global user.email "contact@mrgkanev.eu"
 
 echo "install dropbox"
 
+# Let's install some media stuff
+sudo apt-get install vlc -y
+sudo apt-get install ubuntu-restricted-extras -y
+
+# Customising things
+sudo apt-get install gnome-tweak-tool -y
 
 # So this is still a prototype. Not all of the software is here but im still testing and i got the idea at Saturday 23:30 ... And had to go to a party ...
