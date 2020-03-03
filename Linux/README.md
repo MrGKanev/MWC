12e05758c259133da273ec726be29cbbacb2f995
Gabriel Kanev <contact@mrgkanev.eu>
2020-03-03 10:43:30 +0200

    Updating a bit

--- Linux/README.md
+++ Linux/README.md
@@ -1,8 +1,16 @@
-## Linux (Ubuntu based) version of MWC
+## LUbuntu based version of MWC
 
-## Running
+### Running
 If you want to run the setup.sh you need to download/fork it and change lines 34 and 35. (Git information)
 1. Go to the folder where you downloaded the script
 2. Open terminal and grand root privileges. For Ubuntu it is (sudo -s)
 3. Then run chmod +x setup.sh
 4. After that just ./setup.sh and it will do its magic
+
+## Manjaro based version of MWC
+
+### Running
+If you want to run the setup.sh you need to download/fork it and change lines 35 and 36. (Git information)
+1. Go to the folder where you downloaded the script
+2. Open terminal and grand root privileges. For Ubuntu it is (su -l)
+3. Just run sh setup.sh in /Linux/manjaro/ and it will do its magic
--- Linux/ubuntu/setup.sh
+++ Linux/ubuntu/setup.sh
@@ -8,7 +8,7 @@
 #
 # Version           : 1.0.2
 # Start date        : 28/04/2018
-# Last modified date: 21/05/2018
+# Last modified date: 03/03/2020
 #
 # #################################################################
 
@@ -25,17 +25,13 @@ sudo apt-get dist-upgrade -y
 sudo apt-get upgrade -y
 
 # Install required software
-sudo apt-get install curl git-core build-essential autoconf postgresql mutt parcellite alarm-clock gtimelog irssi shutter htop git-gui vifm libgmp3c2 libgmpxx4ldbl libgmp3-dev imagemagick libmagickcore-dev libmagickcore4 libmagickcore4-extra libmagickwand-dev libmagickwand4 -y
-sudo apt-get install meld autojump trayer xloadimage banshee gnome-do beanstalkd redis-server -y
-sudo apt-get install -y nodejs
-sudo apt-get install -y npm
-sudo apt-get install snapd -y
+sudo apt-get install curl git-core build-essential autoconf postgresql mutt parcellite gtimelog irssi shutter htop git-gui vifm libgmp3c2 libgmpxx4ldbl libgmp3-dev imagemagick libmagickcore-dev libmagickcore4 libmagickcore4-extra libmagickwand-dev libmagickwand4 -y
+sudo apt-get install meld autojump trayer xloadimage banshee gnome-do beanstalkd -y
 
 # These lines need to be changed with your own credentials
 git config --global user.name "mrgkanev"
 git config --global user.email "contact@mrgkanev.eu"
 
-echo "install dropbox"
 
 # Let's install some media stuff
 echo "Installing the media stuff"
@@ -47,21 +43,17 @@ sudo snap install spotify -y
 echo "A bif of customization for Gnome"
 sudo apt-get install gnome-tweak-tool -y
 
-# Adding Sublime text 3 
-echo "Lets install the IDE (sublime-text-3)"
-sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
-sudo apt-get update -y 
-sudo apt-get install sublime-text-installer -y
+# Adding Visual Studio Code
+echo "Lets install the IDE (Visual Studio Code)"
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 # Fixing the Powerline
 echo "Adding the better Powerline"
-sudo apt-add-repository ppa:fish-shell/release-2 -y
-sudo apt-get update -y
+sudo apt install zsh -y
+sudo apt-get install powerline fonts-powerline -y
 sudo apt-get install fish -y
-echo" You need to switch to fish by hand: https://github.com/fish-shell/fish-shell "
-# There will be a solution soon(hopefully)
+echo" You need to switch to fish by hand: https://github.com/robbyrussell/oh-my-zsh.git "
 
 
-
-# So this is still a prototype. Not all of the software is here but im still testing and i got the idea at Saturday 23:30 ... And had to go to a party ... 
-# Edited (21.05.2018): The party was a really good one.
